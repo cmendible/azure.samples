02690e19131df99c10e312dfe6ce9a63599eb6be
cmendible <cmendible@gmail.com>
2020-05-18 16:19:17 +0200

    function connecting to sa with private endpoint

---
+++ function_sa_private_endpoint/deploy/variables.tf
@@ -0,0 +1,15 @@
+variable location {
+  default = "west europe"
+}
+
+variable resource_group {
+  default = "private-endpoint"
+}
+
+variable sa_name {
+  default = "privatecfm"
+}
+
+variable function_required_sa {
+  default = "privatecfmfunc"
+}
