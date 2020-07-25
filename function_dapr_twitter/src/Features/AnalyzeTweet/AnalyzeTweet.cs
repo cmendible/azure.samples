08cc06c51f7953d453005950b8ef7b5a0b676728
cmendible <cmendible@gmail.com>
2020-07-26 00:41:32 +0200

    using cognitive services

--- function_dapr_twitter/src/Features/AnalyzeTweet/AnalyzeTweet.cs
+++ function_dapr_twitter/src/Features/AnalyzeTweet/AnalyzeTweet.cs
@@ -5,6 +5,10 @@ namespace Function.ReadTwitter
     using System.Threading.Tasks;
     using CloudNative.CloudEvents;
     using Microsoft.Extensions.Logging;
+    using System.Collections.Generic;
+    using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+    using Microsoft.Azure.CognitiveServices.Language.LUIS.Authoring;
+    using System.Text.Json;
 
     public static class AnalyzeTweet
     {
@@ -14,9 +18,19 @@ namespace Function.ReadTwitter
         [FunctionName("AnalyzeTweet")]
         public static async Task Run(
             [DaprTopicTrigger(Topic = "feed")] CloudEvent @event,
+            [DaprSecret("demosecrets", "cognitiveServicesKey")] IDictionary<string, string> secret,
             ILogger log)
         {
-            log.LogInformation($"RECEIVED {@event.Data}");
+            var credentials = new ApiKeyServiceClientCredentials(secret["cognitiveServicesKey"]);
+            var client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = "https://westeurope.api.cognitive.microsoft.com/"
+            };
+
+            var tweet = JsonSerializer.Deserialize<Tweet>(@event.Data.ToString());
+
+            var result = await client.SentimentAsync(tweet.Text);
+            log.LogInformation($"Sentiment Score: {result.Score:0.00}");
         }
     }
 }
