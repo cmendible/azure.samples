4e05b85aaa9ece153a513c21660905a854df9875
cmendible <cmendible@gmail.com>
2021-04-28 17:22:54 +0200

    comments and references

--- aks_csi_sa_private_enpoint/terraform/install.sh
+++ aks_csi_sa_private_enpoint/terraform/install.sh
@@ -5,6 +5,7 @@ set -e
 echo "Getting AKS credentials..."
 az aks get-credentials -n $AKS_NAME -g $AKS_RG --overwrite-existing
 
+# Based on: https://github.com/kubernetes-sigs/azurefile-csi-driver/blob/master/docs/install-csi-driver-v1.1.0.md
 echo "Installing azure-file-csi driver"
 curl -skSL https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/v1.1.0/deploy/install-driver.sh | bash -s v1.1.0 --
 
