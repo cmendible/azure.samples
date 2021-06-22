3d97ad53f57be1fde904cf10291af1870ab0b3e0
cmendible <cmendible@gmail.com>
2021-06-22 22:06:06 +0200

    fixed dup name

--- signalr/deploy/signalr.tf
+++ signalr/deploy/signalr.tf
@@ -48,7 +48,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "signalr" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "signalr_clients" {
-  name                  = "signalr"
+  name                  = "signalr_clients"
   resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.signalr.name
   virtual_network_id    = azurerm_virtual_network.vnet_clients.id
