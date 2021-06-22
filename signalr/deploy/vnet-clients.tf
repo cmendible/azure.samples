39902722222e8f35c9f7f1244092c38863b81866
cmendible <cmendible@gmail.com>
2021-06-22 20:33:07 +0200

    changed clients address space

--- signalr/deploy/vnet-clients.tf
+++ signalr/deploy/vnet-clients.tf
@@ -2,12 +2,12 @@ resource "azurerm_virtual_network" "vnet_clients" {
   name                = "aks-vnet-clients"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "aks_subnet_clients" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet_clients.name
-  address_prefixes     = ["10.0.0.0/22"] // Making room for more than 1000 clients
+  address_prefixes     = ["10.1.0.0/22"] // Making room for more than 1000 clients
 }
