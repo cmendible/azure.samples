1f131d0995c53db225e99d619523e5fab5e89e11
cmendible <cmendible@gmail.com>
2021-06-22 20:33:23 +0200

    added private endpoint

--- signalr/deploy/signalr.tf
+++ signalr/deploy/signalr.tf
@@ -13,3 +13,43 @@ resource "azurerm_signalr_service" "signalr" {
     value = "Default"
   }
 }
+
+# Create the privatelink.service.signalr.net Private DNS Zone
+resource "azurerm_private_dns_zone" "signalr" {
+  name                = "privatelink.service.signalr.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_endpoint" "signalr_endpoint" {
+  name                = "signalr-endpoint"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.signalr_subnet.id
+
+  private_service_connection {
+    name                           = "signalr-peconnection"
+    private_connection_resource_id = azurerm_signalr_service.signalr.id
+    is_manual_connection           = false
+    subresource_names              = ["SignalR"]
+  }
+
+  private_dns_zone_group {
+    name = "privatelink-signalr"
+    private_dns_zone_ids = [azurerm_private_dns_zone.signalr.id]
+  }
+}
+
+# Link the Private Zone with the VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "signalr" {
+  name                  = "signalr"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.signalr.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "signalr_clients" {
+  name                  = "signalr"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.signalr.name
+  virtual_network_id    = azurerm_virtual_network.vnet_clients.id
+}
