002554dafeddcdab038965a6b11e5e06cdf6bd8f
cmendible <cmendible@gmail.com>
2022-01-06 10:43:53 -0500

    added api_client_id as output and documentation

--- aad_protect_web_api/deploy/main.tf
+++ aad_protect_web_api/deploy/main.tf
@@ -10,13 +10,13 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
   features {}
 }
 
 data "azurerm_client_config" "current" {}
 
+// This is the AAD Application Registration for the API
 resource "azuread_application" "api" {
   display_name    = "passport-test-api"
   identifier_uris = ["api://passport-test-api"]
@@ -31,6 +31,7 @@ resource "azuread_application" "api" {
     value                = "User"
   }
 
+  // Add access to User.Read.All (Microsoft Graph)
   required_resource_access {
     resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph
 
@@ -44,10 +45,12 @@ resource "azuread_application" "api" {
     mapped_claims_enabled          = true
     requested_access_token_version = null
 
+    // Add our sample client as a known Application
     known_client_applications = [
       azuread_application.client.application_id
     ]
 
+    // This is the scope we'll validate in our APIs
     oauth2_permission_scope {
       admin_consent_description  = "Allow the application to access example on behalf of the signed-in user."
       admin_consent_display_name = "Access example"
@@ -59,9 +62,11 @@ resource "azuread_application" "api" {
   }
 }
 
+// This is the AAD Application Registration for the client.
 resource "azuread_application" "client" {
   display_name = "passport-client"
 
+  // We'll be using a PowerShell client
   public_client {
     redirect_uris = [
       "http://localhost/",
@@ -74,6 +79,7 @@ resource "azuread_application" "client" {
     requested_access_token_version = null
   }
 
+  // You can also use Gisela's web client
   web {
     redirect_uris = [
       "http://localhost:8000/give/me/the/code"
@@ -81,15 +87,22 @@ resource "azuread_application" "client" {
   }
 }
 
+// Pre authorize our client
 resource "azuread_application_pre_authorized" "pre_authorized" {
   application_object_id = azuread_application.api.object_id
   authorized_app_id     = azuread_application.client.application_id
   permission_ids        = ["a7ef8bb6-5085-49a1-b803-517b5a439668"]
 }
 
+// You'll need the following output values to configure your application na use the PowerShell client
 output "tenant_id" {
   description = "TENANT_ID"
   value       = data.azurerm_client_config.current.tenant_id
+}
+
+output "api_client_id" {
+  description = "API CLIENT_ID"
+  value       = azuread_application.api.application_id
 }
 
 output "client_id" {
