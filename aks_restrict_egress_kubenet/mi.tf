d5405b719386d1c7c9b6004f83b81425c0025e90
cmendible <266546+cmendible@users.noreply.github.com>
2022-09-12 20:19:30 +0200

    new sample

---
+++ aks_restrict_egress_kubenet/mi.tf
@@ -0,0 +1,5 @@
+resource "azurerm_user_assigned_identity" "mi" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  name                = "aks-restrict-egress"
+}
