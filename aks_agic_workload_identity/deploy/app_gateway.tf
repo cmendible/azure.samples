2bffc4859b5877cfa56bbc951ad3a3ccf7ea171d
cmendible <266546+cmendible@users.noreply.github.com>
2024-05-27 12:31:54 +0200

    fixed public ip dependency

--- aks_agic_workload_identity/deploy/app_gateway.tf
+++ aks_agic_workload_identity/deploy/app_gateway.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "gateway" {
   name                = "${var.gateway_name}-pip"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
   allocation_method   = "Static"
 }
