8cd4b03a6433eaf981fb167cb1daaddb5fe0e963
cmendible <266546+cmendible@users.noreply.github.com>
2024-12-25 11:50:05 +0000

    removing CriticalAddonsOnly taint from default node pool

--- aks_static_egress_gateway/main.tf
+++ aks_static_egress_gateway/main.tf
@@ -52,7 +52,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     name                         = "default"
     node_count                   = 1
     vm_size                      = "Standard_DS2_v2"
-    only_critical_addons_enabled = true
     upgrade_settings {
       drain_timeout_in_minutes      = 0
       max_surge                     = "10%"
